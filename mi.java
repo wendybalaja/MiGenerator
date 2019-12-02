93234270893fe8d0809344a7d4d04af14bc746d8
wendybalaja <33009822+wendybalaja@users.noreply.github.com>
2019-12-01 20:15:29 -0500

    updated functions

--- mi.java
+++ mi.java
@@ -7,11 +7,28 @@ class mi{
 	public static void main(String[] args){
 
 		Scanner scan = new Scanner(System.in);
-		System.out.println("How long is the string? ");
-		String input = scan.nextLine();
-		int num = Integer.parseInt(input);
+		System.out.println("Type in number to select:");
+		System.out.println("1) Get a mi name");
+		System.out.println("2) Feel like eating something");
+		int sel = Integer.parseInt(scan.nextLine());
 
-		System.out.println(generateString(num));
+		if(sel == 1){
+			System.out.println("How long is the string? ");
+			int num = Integer.parseInt(scan.nextLine());
+			System.out.println(generateString(num));
+		}
+		if(sel == 2){
+			generateEat(scan);
+		}
+	}
+
+	public static void generateEat(Scanner scan){
+			System.out.println("想吃点什么?");
+			String eat = scan.nextLine();
+			System.out.println("(*￣︶￣*)  搞点"+eat+"吃吃 !");
+			System.out.println("想喝点什么?");
+			String drink = scan.nextLine();
+			System.out.println("(*￣︶￣*)  搞杯"+drink+"喝喝 ! ");
 	}
 
 	public static String generateString(int num){
